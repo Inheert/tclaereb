0b1b51dd1b8c1f8a85f1db3884b5b0da44d8946a
Inheert <theoclaereboudt@gmail.com>
2026-06-17 19:07:59 +0200

    push

--- app/main.py
+++ app/main.py
@@ -7,5 +7,5 @@ def root():
     return {"Hello", "pretty"}
 
 @app.get("/crazy-endpoint")
-def crazy_function(useless_id: int, useless_str: str | None = None):
-    return {"id": useless_id, "str": useless_str}, 502
+def crazy_function(useless_id: int, useless_str: str):
+    return {"id": useless_id, "str": useless_str}, 200
